017aca7398b6c2ddfba261af1bb063def471d318
Philippe Comte <philippe_comte@laposte.net>
2018-06-15 11:53:50 +0200

    Add files via upload

---
+++ image-analysis-master/config.js
@@ -0,0 +1,22 @@
+var config = {
+	watson: {
+		language_translator: {
+			url:'',
+			username: '',
+			password: '',
+			version: 'v2'
+		},
+		visual_recognition: {
+			url:'',
+			api_key: '',
+			version_date: '2016-05-20'
+		},
+		text_to_speech: {
+			url:'',
+			username: '',
+			password: '',
+			version: 'v1'
+		}
+	}
+};
+module.exports = config;
