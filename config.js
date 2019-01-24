c43a9b8e0c5b4f5a86fb33e7d7b1921ccd62457d
Philippe Comte <philippe_comte@laposte.net>
2019-01-24 14:19:25 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+ar config = {
+	watson: {
+		language_translator: {
+			url: '',
+			username: '',
+			password: '',
+			version: 'v2'
+		},
+		visual_recognition: {
+			url: '',
+			api_key: '',
+			version_date: ''
+		},
+		text_to_speech: {
+			url: '',
+			username: '',
+			password: '',
+			version: ''
+		}
+	}
+};
+module.exports = config;
